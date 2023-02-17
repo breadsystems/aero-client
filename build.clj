ede28f609da682654d8413fb92874ffc62919796
Coby Tamayo <coby@tamayo.email>
2023-02-17 01:49:06 -0800

    test patch-version

--- build.clj
+++ build.clj
@@ -28,6 +28,8 @@
 
 (defn deploy [_]
   (println "Deploying to Clojars...")
+  (prn 'patch patch-version)
+  #_
   (dd/deploy {:installer :remote
               :artifact jar-file
               :pom-file (b/pom-path {:lib 'systems.bread/harrow
